004ead40f54d14b6832011479db5a56927bf3ad8
gerarjon <gerarsuaverdez@gmail.com>
2022-07-04 17:09:31 -0700

    serve static asset

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
+// Serve up static assets 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 db.sequelize.sync().then(()=>{
   app.listen(PORT, () => {
